5700d125c5b25aced9b6b545d40ff6e501c69ea4
Algama17 <albertogarciamallada@gmail.com>
2025-02-06 14:36:44 +0100

    TEST[ReleaseGhostById] failed

--- src/test/java/dev/ghostbusters/GhostControllerTest.java
+++ src/test/java/dev/ghostbusters/GhostControllerTest.java
@@ -1,6 +1,10 @@
 package dev.ghostbusters;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,5 +29,16 @@ public class GhostControllerTest {
         assertEquals(1, capturedGhosts.size());
         assertEquals("Poltergeist", capturedGhosts.get(0).getName());
     }
-    
+
+    @Test
+    public void testReleaseGhostById(){
+        ghostController.captureGhost("Espíritu del Pescador de Lastres", "Clase IV", "Bajo", "Aparecer durante tormentas en la costa");
+        List<Ghost> allGhosts = ghostController.getGhosts();
+        Ghost ghostToRelease = allGhosts.stream()
+        .filter(ghost -> ghost.getClassType().equalsIgnoreCase("Class B")) .findFirst().orElse(null);
+        assertNotNull(ghostToRelease);
+        assertTrue(ghostController.releaseGhostById(ghostToRelease.getId()));
+        assertFalse(ghostController.releaseGhostById(ghostToRelease.getId()));
+    }
+        
 }
