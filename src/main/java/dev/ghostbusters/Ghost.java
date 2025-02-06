ecf1f01306d9ff5aced34a752ae70bf168d7a927
Akalchi <alejandrafdez1699@gmail.com>
2025-02-06 11:10:28 +0100

    "FEAT [dangerLevel/getDangerLevel] to fix failing tests"

--- src/main/java/dev/ghostbusters/Ghost.java
+++ src/main/java/dev/ghostbusters/Ghost.java
@@ -5,11 +5,13 @@ public class Ghost {
     private int id;
     private String name;
     private String classType;
+    private String dangerLevel;
     
     public Ghost(int id, String name, String classType, String dangerLevel, String specialAbility, Object captureDate) {
         this.id = id;
         this.name = name;
         this.classType = classType;
+        this.dangerLevel = dangerLevel;
     }
 
     public int getId() {
@@ -23,5 +25,9 @@ public class Ghost {
         return classType;
     }
 
-    
+    public String getDangerLevel() {
+        return dangerLevel;
+    }
+
+
 }
