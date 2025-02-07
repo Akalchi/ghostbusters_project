6afd1374d45d3e02a8a89785f75f57e021a8f703
Akalchi <alejandrafdez1699@gmail.com>
2025-02-07 12:58:09 +0100

    "changes on files"

--- src/test/java/dev/ghostbusters/Tests/GhostControllerTest.java
+++ src/test/java/dev/ghostbusters/Tests/GhostControllerTest.java
@@ -1,4 +1,4 @@
-package dev.ghostbusters;
+package dev.ghostbusters.Tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
